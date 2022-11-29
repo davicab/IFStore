aa513b2c38019fb914e3c09c243387bd58a32a21
davicab <davicabral01@gmail.com>
2022-11-29 17:20:44 -0300

    changes

--- main.js
+++ main.js
@@ -10,7 +10,18 @@ switch (currentPage){
         callStored()
         getSelect()
     break
+    case 'cart':    
+        // getSelect()
+        callStored()
+        getSum()
+    break
+    case 'profile':
+        handleProfile()
+    break
 }
+
+let ball = document.querySelector('.mini-ball')
+
 function openMenu(){
     let menu = document.querySelector(".menu-mobile")
     let side = document.querySelector(".side-bar")
@@ -100,18 +111,106 @@ function store(){
     let storedCart = localStorage.getItem("selecionado");
     
     localStorage.setItem(storedCart, add)
+
+    ball.style.opacity = "1"
+
 }
 function callStored(){
-    for ( var i = 0; i < localStorage.length; ++i ) {
-        let guardados = localStorage.getItem(localStorage.key( i ));
-        if( guardados == "adicionado"){
-            let ableItem = document.getElementById(localStorage.key( i ));
-            ableItem.style.display = "flex"
+    if(localStorage.length > 0){
+        for ( var i = 0; i < localStorage.length; ++i ) {
+            let guardados = localStorage.getItem(localStorage.key( i ));
+            let notific = document.querySelector(".notification")
+            let total = document.querySelector(".cart-items")
+
+            if( guardados == "adicionado"){
+                var ableItem = document.getElementById(localStorage.key( i ));
+                ableItem.classList.remove("d-none")
+                if(notific){
+                    notific.classList.add("d-none")
+                    total.classList.remove("d-none")
+                }
+            }if( guardados == "selecionado"){
+                if(notific){
+                    notific.classList.remove("d-none")
+                    total.classList.add("d-none")
+                }
+            }
         }
     }
 }
 function deleteItem(e){
     let element = e.parentNode
     let parentElement = element.parentNode
-    console.log(parentElement.id)
+    let textDel = document.querySelector(".getId")
+    textDel.innerHTML = `${parentElement.id}`
+    let popDel = document.querySelector(".popDelete")
+    popDel.classList.remove("moveOut")
+
+    let yesDel = document.querySelector(".del-yes")
+    let noDel = document.querySelector(".del-no")
+
+    yesDel.addEventListener(("click"), () =>{
+        localStorage.removeItem(parentElement.id)
+        window.location.reload();
+        popDel.classList.add("moveOut")
+    })
+
+    noDel.addEventListener(("click"), () =>{
+        popDel.classList.add("moveOut")
+    })
+
+}
+setInterval(function() {
+    for ( var i = 0; i < localStorage.length; ++i ) {
+        let guardados = localStorage.getItem(localStorage.key( i ));
+        if( guardados == "adicionado"){
+            ball.style.opacity = "1"
+        }
+        
+    }
+}, 500);
+
+function getSum(){
+    let all_items = document.querySelectorAll(".selected-item")
+    let valor = null;
+    parseInt(valor)
+    if(!all_items[0].classList.contains("d-none")){
+        let value0 = all_items[0].dataset.value
+        let string0 = parseInt(value0)
+        valor += string0
+    }
+    if(!all_items[1].classList.contains("d-none")){
+        let value1 = all_items[1].dataset.value
+        let string1 = parseInt(value1)
+        valor += string1
+    }
+    if(!all_items[2].classList.contains("d-none")){
+        let value2 = all_items[2].dataset.value
+        let string2 = parseInt(value2)
+        valor += string2
+    }
+    let send_value = document.getElementById("receive-value")
+    send_value.innerHTML = valor
+    console.log(valor)
+}
+
+function handleProfile(){
+    let adress = document.getElementById("adress")
+    let payment = document.getElementById("payment")
+
+    let form_adress = document.querySelector(".adress-form")
+    let form_payment = document.querySelector(".payment-form")
+
+    adress.addEventListener(("click"), () =>{
+        if(!form_payment.classList.contains("invisible")){
+            form_payment.classList.add("invisible")
+        }
+        form_adress.classList.remove("invisible")
+    })
+    payment.addEventListener(("click"), () =>{
+        if(!form_adress.classList.contains("invisible")){
+            form_adress.classList.add("invisible")
+        }
+        form_payment.classList.remove("invisible")
+    })
 }
